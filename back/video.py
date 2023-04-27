b8f62102a1829899b839dbe3b2bb42ab44136ab1
Darth_Maul <n.chistoprudov@yandex.ru>
2023-04-27 21:59:19 +0300

    feat: :zap: добавлена точка входа
    добавлена точка входа

--- back/video.py
+++ back/video.py
@@ -54,4 +54,5 @@ def video():
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-app.run(debug=True, host="0.0.0.0", port=5001)
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5001)
